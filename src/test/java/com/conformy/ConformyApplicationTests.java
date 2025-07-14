722153849c9b33888b8f77fd2dc2f78329294976
Isabela Felix <isabbelafelix@gmail.com>
2025-07-14 14:08:23 -0300

    Inicializa projeto Spring Boot base

---
+++ src/test/java/com/conformy/ConformyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.conformy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConformyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
